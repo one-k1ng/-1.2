35913d0749b88fa84878264bb274b4964a5abcb3
Администратор <@DESKTOP-PHORFLN>
2020-09-08 17:16:17 +0300

    123

---
+++ Task2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Практика1._2
+{
+    class Task2
+    {
+        static string s = "Microsoft .NET Framework 2 Application Development Foundation";
+        static string[] sa = s.Split(" ");
+        static void Main(string[] args)
+        {
+            Array.Sort(sa);
+            s = string.Join(" ", sa); Console.WriteLine(s);
+        }
+    }
+}
