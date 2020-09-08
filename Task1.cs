37d67df42bcae4530734c7137d2e69b99f139e85
Администратор <@DESKTOP-PHORFLN>
2020-09-08 16:52:26 +0300

    Добавьте файлы проекта.

---
+++ Task1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Практика1._2
+{
+    class Task1
+    {
+        static SByte a = 0;
+        static Byte b = 0;
+        static Int16 c = 0;
+        static Int32 d = 0;
+        static Int64 e = 0;
+        static string s = "";
+        static Exception ex = new Exception();
+        static object[] types = { a, b, c, d, e, s, ex };
+        static void Main(string[] args)
+        {
+            foreach (object o in types)
+            {
+                string type;
+                if (o.GetType().IsValueType) type = "Value type";
+                else
+                    type = "Reference Type";
+                Console.WriteLine("{0}: {1}", o.GetType(), type);
+            };
+        }
+    }
+}
